e2c985d5ce5c711e452a928d7962da11aef111a4
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2025-05-06 13:00:32 -0400

    initial commit

---
+++ server/src/main.rs
@@ -0,0 +1,53 @@
+use std::{io, thread};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::time::{Duration, SystemTime};
+
+fn server_connection(stream: &mut TcpStream) {
+    println!("inside server_connection!");
+    stream.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
+    stream.set_write_timeout(Some(Duration::from_secs(2))).unwrap();
+
+    loop {
+        let mut buffer = String::new();
+        let mut bff = BufReader::new(stream.try_clone().unwrap());
+
+        match bff.read_line(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!("Client disconnected (red 0 bytes)!");
+                    return;
+                }
+
+                println!("Got a message ({:?} bytes) : {:?}", size, buffer);
+                stream.write(buffer.as_bytes())
+                    .expect("could not write to socket");
+            }
+
+            Err(error) => {
+                println!("Could not read from stream : {:?}", error);
+                println!("Leaving function...");
+                return;
+            }
+        }
+
+
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080")
+                                .unwrap_or_else(|e| panic!("could not bind : {:?}", e));
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut s) => {
+                thread::spawn(move || server_connection(&mut s));
+            }
+
+            Err(e) => {
+                panic!("could not serve connection : {:?}", e)
+            }
+        }
+    }
+}
