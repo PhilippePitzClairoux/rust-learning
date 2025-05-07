c16d9aa67e76087496388ef9fc800175193ab304
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2025-05-06 21:58:11 -0400

    lib didnt actually push - oof...

---
+++ local_utils/src/lib.rs
@@ -0,0 +1,45 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpStream;
+
+pub mod tcp {
+    use std::fmt;
+    use super::*;
+
+    #[derive(Debug, Clone)]
+    pub struct ConnectionClosed;
+
+    impl fmt::Display for ConnectionClosed {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "connection closed")
+        }
+    }
+
+    pub fn get_message(sock: &mut TcpStream) -> Result<String, ConnectionClosed> {
+        let mut buffer = String::new();
+        let mut reader = BufReader::new(sock.try_clone().unwrap());
+
+        match reader.read_line(&mut buffer) {
+            Ok(len) => {
+                if len == 0 {
+                    return Err(ConnectionClosed)
+                }
+
+                Ok(buffer)
+            }
+            Err(e) => {
+                panic!("could not read from host - server might not be running: {}", e)
+            }
+        }
+    }
+
+    pub fn send_message(sock: &mut TcpStream, msg: &str) {
+        match sock.write_all(&msg.as_bytes()) {
+            Ok(_) => {
+                println!("Sent : {:?}", &msg)
+            }
+            Err(e) => {
+                panic!("could not write to host - server might not be running: {}", e)
+            }
+        }
+    }
+}
