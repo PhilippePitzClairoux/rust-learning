d5dcc499622d2887201cc2375be79f3e28c7bad6
Philippe Pitz Clairoux <philippe.pitz.clairoux@usherbrooke.ca>
2025-05-30 15:33:09 -0400

    Add more doc and add a script to generate files/directories on the go to test encryption

--- rustware/src/files.rs
+++ rustware/src/files.rs
@@ -5,16 +5,30 @@ use std::path::{Path, PathBuf};
 use lazy_static::lazy_static;
 use tempfile::{NamedTempFile, TempDir};
 use crate::errors::File as FileError;
-use crate::files;
+
 
 lazy_static! {
+    /// Points to the OS default temp directory - this is a fallback in case we can't get parent directory
+    /// for a specific file.
     static ref DEFAULT_TEMP_DIR: PathBuf = env::temp_dir();
 }
-
 
 // Default chunk size - 5 MB
 pub const FILE_CHUNK_SIZE: u64 = 1024 * 1024;
 
+/// Open a file for reading purposes. Returns a wrapped error if we can't.
+///
+/// # Errors
+///   * if we don't have the permissions to read file
+///
+/// # Example
+/// ```no_run
+/// use std::path::Path;
+/// use rustware::files::open_file;
+///
+/// let shadow_reader = open_file(Path::new("/etc/shadow")).unwrap();
+/// println!("{:?}", shadow_reader)
+/// ```
 pub fn open_file<P: AsRef<Path>>(path: P) -> Result<File, FileError> {
     match File::open(path) {
         Ok(file) => Ok(file),
@@ -22,6 +36,22 @@ pub fn open_file<P: AsRef<Path>>(path: P) -> Result<File, FileError> {
     }
 }
 
+/// Create a new file (for writing purposes) and return a handle to that file.
+///
+/// # Errors
+///   * if we can't create a file in the specified location
+///   * if the file already exists
+///
+/// # Examples
+///
+/// ```no_run
+/// use std::io::Write;
+/// use std::path::Path;
+/// use rustware::files::create_file;
+///
+/// let mut f = create_file(Path::new("/tmp/wow.txt")).unwrap();
+/// f.write_all(b"Damn right!").unwrap();
+/// ```
 pub fn create_file<P: AsRef<Path>>(path: P) -> Result<File, FileError> {
     match File::create(path) {
         Ok(file) => Ok(file),
@@ -29,6 +59,20 @@ pub fn create_file<P: AsRef<Path>>(path: P) -> Result<File, FileError> {
     }
 }
 
+/// Read a vector of `size` bytes from `file` and return its content.
+///
+/// # Errors
+///   * if the file can't be read anymore
+///
+/// # Example
+///
+/// ```no_run
+/// use std::path::Path;
+/// use rustware::files::{open_file, read_chunk};
+///
+/// let mut shadow_reader = open_file(Path::new("/etc/shadow")).unwrap();
+/// let chunk = read_chunk(&mut shadow_reader, 4096); // read 4096 bytes from shadow_reader
+/// ```
 pub fn read_chunk<R>(file: &mut R, size: usize) -> Result<Vec<u8>, FileError>
 where
     R: Read,
@@ -39,6 +83,21 @@ where
     Ok(buffer)
 }
 
+/// Write a `data` (aka `chunk` aka vector of bytes) to target `file`
+/// 
+/// # Errors
+///   * I/O errors while writing
+/// 
+/// # Examples
+/// 
+/// ```no_run
+/// use std::io::Write;
+/// use std::path::Path;
+/// use rustware::files::{create_file, write_chunk};
+///
+/// let mut f = create_file(Path::new("/tmp/wow.txt")).unwrap();
+/// write_chunk(&mut f, "oh no... why all this wrapping!!!".as_bytes()).unwrap();
+/// ```
 pub fn write_chunk<W>(file: &mut W, data: &[u8]) -> Result<(), FileError>
 where
     W: Write,
@@ -47,6 +106,20 @@ where
     Ok(())
 }
 
+/// Rename `input` to `output`. This will completely erase the original content of `output`.
+/// The `input` file will be moved to the location of `output` with the exact same name.
+///
+/// # Errors
+///   * if one of the files is deleted beforehand
+///   * if the files aren't on the same device (linux only I belive)
+/// 
+/// # Examples
+/// ```no_run
+/// use std::path::Path;
+/// use rustware::files::replace_file;
+///  
+/// replace_file(Path::new("/tmp/a.xt"), Path::new("/tmp/b.txt")).unwrap();
+/// ```
 pub fn replace_file<P: AsRef<Path>, Q: AsRef<Path>>(input: P, output: Q) -> Result<(), FileError> {
     fs::rename(input, output)?;
     Ok(())
